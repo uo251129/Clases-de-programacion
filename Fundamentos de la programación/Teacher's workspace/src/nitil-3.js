9e6425dcab05f8769d9ac8f829b4b9005f589ee7
LuisValls-Ironhack <84102901+LuisValls-Ironhack@users.noreply.github.com>
2021-07-29 13:24:26 +0200

    Update nitil-3.js

--- Fundamentos de la programación/Teacher's workspace/src/nitil-3.js
+++ Fundamentos de la programación/Teacher's workspace/src/nitil-3.js
@@ -17,6 +17,23 @@ while (productos <= 1000){
     productos = 2 * productos;
     
 }
-
+//primero tengo que utilizar el valor que estoy modificando y despues modificarlo!!!!!!
 console.log(arrayOfProducts);
 
+/*Define una array cuyo primer elemento sea el número 1000, y que sus siguientes elementos sean la mitad del anterior, tal que:
+[1000, 500, 250...]
+Todos los valores serán mayor que uno, de forma que se parará de añadir elementos a esta array cuando la siguiente división supondría un valor menor que uno.
+Y luego imprime la array por consola */
+
+var productosADividir = 1000;
+var arrayOfNumeritos = [];
+
+while (productosADividir >= 1){
+    arrayOfNumeritos.push(productosADividir);
+    productosADividir = productosADividir / 2;
+}
+
+console.log(arrayOfNumeritos)
+
+
+  
