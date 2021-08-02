7f816160086e09bd2a37576be8c89024e5bb75c6
Francisco Manuel Mendoza Soto <franciscomanuelmendozasoto@gmail.com>
2021-08-02 10:55:36 +0200

    Meeting functions

---
+++ Fundamentos de la programación/Teacher's workspace/src/functions.js
@@ -0,0 +1,53 @@
+
+function f0(x0, x1){
+    var y = x0 + x1**2;
+    
+    return y;
+}
+
+var y = f0(2, 3);
+
+console.log(y);
+
+
+function lastElementOfArray(array){
+    return array[array.length - 1];
+}
+
+var fruit = ['apple', 'banana', 'coconut', 'orange', 'strawberry'];
+
+var lastFruit = lastElementOfArray(fruit)
+
+console.log(lastFruit);
+
+
+var clothesArray = []
+
+function addClothes(clothes){
+    console.log("What a nice " + clothes);
+    clothesArray.push(clothes);
+}
+
+addClothes('jeans');
+addClothes('T-shirt');
+addClothes('shoes');
+addClothes('boxers');
+addClothes('scarf');
+
+console.log(clothesArray);
+
+
+function meanOfArray(array){
+    var totalSum = 0;
+    for(let element of array){
+        totalSum += element;
+    }
+    var mean = totalSum / array.length;
+    return mean;
+}
+
+var numberArray = [4, 2, 3, 1, 2, 2, 3, 24]
+
+var avg = meanOfArray(numberArray);
+
+console.log(avg);
