5503a6d1c99dddd364b392d6cef0a597ca4eecaf
Francisco Manuel Mendoza Soto <franciscomanuelmendozasoto@gmail.com>
2021-08-18 09:26:18 +0200

    Tarea 16/08

--- Fundamentos de JQuery/src/jquery2.js
+++ Fundamentos de JQuery/src/jquery2.js
@@ -1,7 +1,7 @@
 //revisar paso a paso la traza del ejercico//
 
 function matrixOfProducts (xSize, ySize) {
-   var matrix = [];
+    var matrix = [];
     for (let i = 0; i < xSize; i++) {
         var row = [];
         for (let j = 0; j < ySize; j++) {
@@ -25,21 +25,40 @@ console.log(result2);
 
 function matrixOfFractions (xSize, ySize) {
     var matrix = [];
-     for (let i = 0; i < xSize; i++) {
-         var row = [];
-         for (let j = 0; j < ySize; j++) {
-             var element = i / (j + 1);
-             row.push(element);   
+    for (let i = 0; i < xSize; i++) {
+        var row = [];
+        for (let j = 0; j < ySize; j++) {
+            var element = i / (j + 1);
+            row.push(element);   
              
-         } 
-         matrix.push(row);
-     }
-     return matrix;
+        } 
+        matrix.push(row);
     }
+    return matrix;
+}
 
-    var result3 = matrixOfFractions(8, 8)
-    console.log(result3)
-    var result4 = matrixOfFractions(4, 12)
-    console.log(result4)
+var result3 = matrixOfFractions(8, 8)
+console.log(result3)
+var result4 = matrixOfFractions(4, 12)
+console.log(result4)
 
-    
+
+function decreaseMatrix(matrix){
+    substractedMatrix = [];
+    for (let i = 0; i < matrix.length; i++){
+        var row = [];
+        for (let j = 0; j < matrix[0].length; j++){
+            var element = matrix[i][j] - 1;
+            row.push(element);
+        }
+        substractedMatrix.push(row);
+    }
+    return substractedMatrix;
+}
+
+
+var result5 = decreaseMatrix(result);
+console.log(result5);
+
+var result6 = decreaseMatrix(result4);
+console.log(result6);   
