4d917c71a121f9bd37fbdee3cb3fbe7effaed82b
Francisco Manuel Mendoza Soto <franciscomanuelmendozasoto@gmail.com>
2021-08-19 09:35:18 +0200

    Revieweing homework 18/08

--- Minesweeper/index.js
+++ Minesweeper/index.js
@@ -2,3 +2,11 @@
 function redirectTo(link){
     window.location.href = link;
 }
+
+function startCustomGame(){
+    var rows = $("#row_input").val();
+    var columns = $("#column_input").val();
+    var mines = $("#mines_input").val();
+
+    redirectTo("src/minesweeper.html?xSize="+rows+"&ySize="+ columns + "&mines=" + mines);
+}
