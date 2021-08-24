dc8fdf44aab87596892f3f4ba2b224a8135dac10
LuisValls-Ironhack <84102901+LuisValls-Ironhack@users.noreply.github.com>
2021-08-24 12:04:28 +0200

    Edited Clock Format

--- Minesweeper/src/clock.js
+++ Minesweeper/src/clock.js
@@ -1,9 +1,12 @@
 var counter = 0;
 var countsUp;
 
+
 function increaseCount(){
     counter++;
-    $("#clock").html(counter);
+
+    $("#clock").html(formatCode(counter));
+
 }
 
 function countUp(){
@@ -13,4 +16,22 @@ function countUp(){
     } 
 
     countsUp = setInterval(increaseCount, 1000);          
+}
+
+function formatCode(seconds){
+    var hours = Math.floor(seconds/3600);
+   var secondsLeft = seconds - (hours*3600);
+    var min = Math.floor(secondsLeft/60);
+    secondsLeft = secondsLeft - 60*min;
+    if (min < 10){
+        min = "0" + min;
+    }
+    if (secondsLeft < 10){
+        secondsLeft = "0" + secondsLeft;
+    }
+    if ( hours < 10){
+        hours = "0" + hours;
+    }
+    var clockString = hours + ":" + min + ":" + secondsLeft;  
+    return clockString;
 }
